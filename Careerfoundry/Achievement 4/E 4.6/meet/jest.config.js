2123c893a5202bb1831d84a07a77b1efeeae0fc2
shadesi <45621420+shadesi@users.noreply.github.com>
2024-09-21 19:45:54 +0200

    Delete Careerfoundry directory

--- Careerfoundry/Achievement 4/E 4.6/meet/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  };
-  
