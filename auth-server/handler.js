1d9876c7cc0ade84ee6524d296a0210cefd9c960
Shane <shane@freenet.de>
2024-11-02 23:56:36 +0100

    Updated handler.js with environment variables and debugging logs

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const { google } = require('googleapis');
 const calendar = google.calendar('v3');
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
